1fe21aa500d9161c53b00147c76ae8e82a8d4e1d
kayla <kay.ann2000@gmail.com>
2022-03-18 10:57:01 -0600

    commit tree 373105aa134779bd8166a4849923a216ba8502ee

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const tutorials = [
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
+
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
@@ -12,5 +13,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  const newArray = tutorials.map(element => {
+  const newArraySplit = element.split(' ');
+  const indiviLetter = newArraySplit.map(x => {
+  const upperCaseLetter =  x.charAt(0);
+  const newLetter = upperCaseLetter.toUpperCase();
+  const restOfWord = x.substring(1);
+  const fullWord = newLetter.concat('', restOfWord);
+  return fullWord;
+})
+ const fullString = indiviLetter.join(' ');
+ return fullString;
+  })
+  return newArray;
+  //return tutorials.map()
 }
+titleCased();
+//console.log(tutorials)
+//console.log(firstLetter)
